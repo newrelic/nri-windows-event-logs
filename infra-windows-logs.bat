1a9aad674afa6b3ff05a7bf69d765fa9c8dd12d7
csandels <3860745+csandels@users.noreply.github.com>
2019-02-28 12:47:26 -0700

    Event filter (#2)
    * Renamed config and def file to be consistent
    
    * Added event filtering & removed semicolons for consistency
    
    * Put back some important notes
    
    * Clean up the param notes

--- infra-windows-logs.bat
+++ infra-windows-logs.bat
@@ -1,3 +1,9 @@
 @ECHO OFF
-set logname=%1
-powershell.exe -noprofile -executionpolicy bypass -file infra-windows-logs.ps1 -LogName %logname%
+set logName=%1
+set eventIds=%2
+
+IF DEFINED eventIds (
+    powershell.exe -ExecutionPolicy Unrestricted -file infra-windows-logs.ps1 -LogName %logName% -EventIds %eventIds%
+) ELSE (
+    powershell.exe -ExecutionPolicy Unrestricted -file infra-windows-logs.ps1 -LogName %logName%
+)
